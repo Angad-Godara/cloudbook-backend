4728212150f7114efec922b59e61f957e3f7748b
Angad-Godara <angadrrr2002@gmail.com>
2023-01-05 15:28:05 +0530

    TRY: trying to fix cors

--- server.js
+++ server.js
@@ -5,10 +5,16 @@ connectToMongo();
 var cors = require('cors')
 
 const app = express()
-app.use(cors())
 const port = 5000
 
 app.use(express.json());
+
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
 
 // Available routes
 app.use('/api/auth', require('./routes/auth'))
